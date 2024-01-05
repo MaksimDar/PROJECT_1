ce789b3affd84d4fe547185d47c7a9a15f4892fb
Maksim Dovhusha <dovgusha.m@gmail.com>
2024-01-05 01:47:23 +0100

    add

--- project_1.C
+++ project_1.C
@@ -2,14 +2,45 @@
 
 int main() {
     char option;
-
+    char local, destination;
+    int alpha, beta, gamma;
 
     printf("Welcome to our building!\n\n");
 
-    printf("A. Use Elevator | B. Statistics | C. Elevators inspection | Q. Quit\n");
-    printf("Enter option: ");
-    scanf("%f", &option);
-
+    do {
+        printf("A. Use Elevator | B. Statistics | C. Elevators inspection | Q. Quit\n");
+        printf("Enter option: ");
+        scanf(" %c", &option);
+        if (option != 'A' && option != 'a' && option != 'B' && option != 'b' && option != 'C' && option != 'c' && option != 'Q' && option != 'q') {
+            printf("(ERROR) Invalid option\n\n");
+        }
+    } while (option != 'A' && option != 'a' && option != 'B' && option != 'b' && option != 'C' && option != 'c' && option != 'Q' && option != 'q');
+    
+  
+    printf("From? ");
+    scanf(" %c", &local);
+    while (!(local == 'A' || local == 'B' || local == 'G' || ( local <= '1' && local >= '5')) ) {
+        printf("From? ");
+        scanf(" %c", &local);
+    }
+    
+    switch(local) {
+        case 'A':
+        case 'G':
+        case 'B':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+            printf("To? ");
+            scanf(" %c", &destination);
+            while (!(destination == 'A' || destination == 'B' || destination == 'G' || ( destination <= '1' && destination >= '5')))  {
+                //  destination != '1' && destination != '2' && destination != '3' && destination != '4' && destination != '5'
+                printf("To? ");
+                scanf(" %c", &destination);
+            }
+            break;
+    }
     return 0;
-
 }
--- tempCodeRunnerFile.C
+++ tempCodeRunnerFile.C
@@ -17,12 +17,11 @@ int main() {
     } while (option != 'A' && option != 'a' && option != 'B' && option != 'b' && option != 'C' && option != 'c' && option != 'Q' && option != 'q');
     
   
-    printf("From? ");
-    scanf(" %c", &local);
-    while (!(local == 'A' || local == 'B' || local == 'G' || ( local <= '1' && local >= '5')) ) {
+   
+    do   {
         printf("From? ");
         scanf(" %c", &local);
-    }
+    } while (local != 'A' && local != 'B' &&  local != 'G' &&  !( local >= '1' && local <= '5'));
     
     switch(local) {
         case 'A':
@@ -35,12 +34,50 @@ int main() {
         case '5':
             printf("To? ");
             scanf(" %c", &destination);
-            while (!(destination == 'A' || destination == 'B' || destination == 'G' || ( destination <= '1' && destination >= '5')))  {
-                //  destination != '1' && destination != '2' && destination != '3' && destination != '4' && destination != '5'
+            while (destination != 'A' && destination != 'G' && destination != 'B' && destination != '1' && destination != '2' && destination != '3' && destination != '4' && destination != '5')  {
+               
                 printf("To? ");
                 scanf(" %c", &destination);
             }
             break;
     }
-    return 0;
+   
+
+    while (local == destination) {
+        printf("(ERROR) Are you joking?\n\n");
+        do {
+            printf("A. Use Elevator | B. Statistics | C. Elevators inspection | Q. Quit\n");
+            printf("Enter option: ");
+            scanf(" %c", &option);
+            if (option != 'A' && option != 'a' && option != 'B' && option != 'b' && option != 'C' && option != 'c' && option != 'Q' && option != 'q') {
+              printf("(ERROR) Invalid option\n\n");
+            }
+        } while (option != 'A' && option != 'a' && option != 'B' && option != 'b' && option != 'C' && option != 'c' && option != 'Q' && option != 'q');
+        do   {
+        printf("From? ");
+        scanf(" %c", &local);
+        switch(local) {
+        case 'A':
+        case 'G':
+        case 'B':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+            printf("To? ");
+            scanf(" %c", &destination);
+            while (destination != 'A' && destination != 'G' && destination != 'B' && destination != '1' && destination != '2' && destination != '3' && destination != '4' && destination != '5')  {
+               
+                printf("To? ");
+                scanf(" %c", &destination);
+            }
+            break;
+    };
+    } while (local != 'A' && local != 'B' &&  local != 'G' &&  !( local >= '1' && local <= '5'));
+    };
+    if (local != destination) {
+        printf("Great job!");
+    };
+ return 0;
 }
